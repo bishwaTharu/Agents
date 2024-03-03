76a8ad9d565075a6a1cd6f04e0f4f1e40bb037f5
bishwaTharu <bishwanaththaru34@gmail.com>
2024-03-03 19:47:17 +0545

    test

--- tests/ollama_init.py
+++ tests/ollama_init.py
@@ -1,1 +1,7 @@
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+key = os.getenv("OllAMA_BASE_URL")
+# print(key)
